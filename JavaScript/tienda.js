16558f46559f3d06cfc953c151b07671db5519dc
SimonLav2024 <simonlav94@gmail.com>
2024-08-29 13:05:49 +0200

    tienda en proceso

--- JavaScript/tienda.js
+++ JavaScript/tienda.js
@@ -1,18 +1,18 @@
 
 
 const products = [
-    { id: 1, name: "BMW Serie 7 740i", price: 5900, image: "../img/cars/740-i-1.jpg", sizes: ["S", "M", "L", "XL"], year: "1994", km: "234000" },
-    { id: 2, name: "Honda Accord Sport 2.0", price: 19.99, image: "../img/camisetaChica2.jpeg", sizes: ["S", "M", "L", "XL"], year: "2006", km: "" },
-    { id: 3, name: "Hyundai Coupe", price: 19.99, image: "../img/camisetaChica3.jpeg", sizes: ["S", "M", "L", "XL"], year: "", km: "" },
-    { id: 4, name: "", price: 19.99, image: "../img/camisetaChica4.jpeg", sizes: ["S", "M", "L", "XL"], year: "", km: "" },
-    { id: 5, name: "", price: 19.99, image: "../img/camisetaChico1.jpeg", sizes: ["S", "M", "L", "XL"], year: "", km: "" },
-    { id: 6, name: "", price: 19.99, image: "../img/camisetaChico2.jpeg", sizes: ["S", "M", "L", "XL"], year: "", km: "" },
-    { id: 7, name: "", price: 19.99, image: "../img/camisetaChico3.jpeg", sizes: ["S", "M", "L", "XL"], year: "", km: "" },
-    { id: 8, name: "", price: 39.99, image: "../img/pantalon.jpeg", sizes: ["28", "30", "32", "34"], year: "", km: "" },
-    { id: 9, name: "", price: 29.99, image: "../img/pantalonPirata.jpeg", sizes: ["28", "30", "32", "34"], year: "", km: "" },
-    { id: 10, name: "", price: 59.99, image: "../img/vestido1.jpeg", sizes: ["38", "39", "40", "41", "42"], year: "", km: "" },
-    { id: 11, name: "", price: 49.99, image: "../img/vestido2.jpeg", sizes: ["38", "39", "40", "41", "42"], year: "", km: "" },
-    { id: 12, name: "", price: 39.99, image: "../img/vestido3.jpeg", sizes: ["38", "39", "40", "41", "42"], year: "", km: "" },
+    { id: 1, name: "BMW Serie 7 740i", price: 5900, image: "../img/cars/740-i-1.jpg", year: "1994", km: "234000" },
+    { id: 2, name: "Honda Accord Sport 2.0", price: 6450, image: "../img/cars/honda-accord.jpg", year: "2006", km: "130000" },
+    { id: 3, name: "Hyundai Coupe 2.0", price: 3500, image: "../img/cars/hyundai-coupe.jpg", year: "2004", km: "156321" },
+    { id: 4, name: "Toyota Celica GTS", price: 7000, image: "../img/cars/toyota-celica.jpg", year: "2005", km: "160000" },
+    { id: 5, name: "Toyota Supra MK4", price: 54000, image: "../img/cars/toyota-supra.jpg", year: "1994", km: "56000" },
+    { id: 6, name: "Mitsubishi Lancer Evo 7", price: 55000, image: "../img/cars/evo-7.jpg", year: "2001", km: "53300" },
+    { id: 7, name: "Subaru Impreza WRX STi", price: 56000, image: "../img/cars/Subaru_WRX_STi.jpg", year: "2004", km: "37890" },
+    { id: 8, name: "Mazda RX7", price: 45000, image: "../img/cars/Mazda-rx7.jpg", year: "1994", km: "78000" },
+    { id: 9, name: "Mazda RX8", price: 10500, image: "../img/cars/Mazda_RX-8.jpg", year: "2007", km: "34500" },
+    { id: 10, name: "Chevrolet Chevelle SS 396", price: 78000, image: "../img/cars/chevelle-70.jpg", year: "1970", km: "25000" },
+    { id: 11, name: "Dodge Charger R/T 440", price: 69000, image: "../img/cars/Dodge-Charger.jpg", year: "1968", km: "67000" },
+    { id: 12, name: "Mercedes-Benz W201", price: 54000, image: "../img/cars/mercedes.jpg", year: "1990", km: "356000" },
 ];
 
 const productosContainer = document.getElementById("products");
@@ -23,7 +23,6 @@ const carrito = document.getElementById("cart");
 const totalCarrito = document.getElementById("cart-total");
 const contador = document.getElementById("contador");
 
-// es importante señalar que todas las variables que esten creadas o mencionadas dentro de cualqueor lado en la funcion hay que ponerla asi: ${variable}.
 function renderizarProducts(){
     productosContainer.innerHTML = products.map(producto => `
         <div class="product-card">  
@@ -32,22 +31,19 @@ function renderizarProducts(){
             <h4>Del año ${producto.year}</h4>
             <h4>${producto.km} kilómetros</h4>
             <p>Precio:<b> ${producto.price} €</b></p>
-            <button onclick="addCarrito(${producto.id})">Añadir al carrito</button>
+            <button onclick="addCarrito(${producto.id})">Comprar</button>
         </div>`
     ).join(" ");
 }
 function addCarrito(productoId){
    const productoComprado = products.find(producto => producto.id === productoId);
-   const size = document.getElementById(`size-${productoComprado.id}`).value;
-// para que se refleje la variable funcional las comillas tienes que ser las torcidas esas de al lado de la p: (``) porque si pones de las otras cualquier otras no va a funcionar la variable.
-    cardProducts.push({...productoComprado, size});
-    
-    updateCarrito(); //esto es llamar la funcion para que se muestre
+    cardProducts.push({...productoComprado});
+    updateCarrito();
 }
 function updateCarrito(){
     itemsCarrito.innerHTML = cardProducts.map((item, index) => 
         `<div class="cart-item">
-            <span>${item.name} ${item.size} - ${item.price.toFixed(2)} €</span>
+            <span>${item.name} - ${item.price.toFixed(2)} €</span>
             <button onclick="eliminarCarrito(${index})">Eliminar</button>
         </div>`).join(" ");
 
@@ -60,7 +56,6 @@ function updateCarrito(){
         contador.textContent = cardProducts.length;
     }
     guardarCarrito();
-   
 }
 function eliminarCarrito(indice){
     cardProducts.splice(indice, 1);
@@ -70,7 +65,7 @@ mostrarCarrito.addEventListener('click', () => {
     carrito.classList.toggle("open");
 });
 function guardarCarrito(){
-    localStorage.setItem("clave", JSON.stringify(cardProducts)); // el JSON es para transformar el array en un archivo json.
+    localStorage.setItem("clave", JSON.stringify(cardProducts));
 }
 function cargarCarrito(){
     const carritoGuardado = localStorage.getItem("clave");
@@ -79,8 +74,6 @@ function cargarCarrito(){
         updateCarrito();
     }
 }
-
 renderizarProducts();
 cargarCarrito();
-
   
